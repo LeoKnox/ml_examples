14a0b74e5bfa1c42edbd278a8b54e59800e9afae
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-06 23:01:27 -0700

    Add files via upload

--- regression_intro.py
+++ regression_intro.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import quandl
+import math
 
-#df = Quandl.get('FINRA/FORF_TLLTD', start_date='2018-06-22', end_date='2018-06-22')
-df = quandl.get('WIKI/GOOGL')
+df = quandl.get('FINRA/FORF_TLLTD', start_date='2018-06-22', end_date='2018-06-22')
+#df = quandl.get('WIKI/GOOGL')
 
 df = df[[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj Volume']]]
 df['HL_PCT'] = (df['Adj. High'] -  df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] *100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-9999, inplace=True)
+
+forecast_out = int(math.ceil(0.01&len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 
 print(df.head())
